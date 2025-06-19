b9c37eafa3aabb97c0f6591957948b1f356d98a3
desmoris <damianesmoris@gmail.com>
2025-06-19 17:47:17 -0300

    `Task.status` pasó a ser  de tipo `VARCHAR(128)` y se corrigieron algunas restircciones mal definidas en `TasksHistory` y `TasksAssignations`

--- structure/1-tables.sql
+++ structure/1-tables.sql
@@ -10,23 +10,20 @@ CREATE TABLE Users(
     email VARCHAR(64)
 );
 
--- status:
--- * not started = NULL
--- * in progress = 0
--- * complete = 1
 CREATE TABLE Tasks(
     id INT IDENTITY (1,1) PRIMARY KEY,
     title VARCHAR(255) UNIQUE,
     description TEXT,
     dateTime DATETIME,
-    status BIT DEFAULT NULL  
+    status VARCHAR(128) DEFAULT NULL  
 );
 
 CREATE TABLE TasksAssignations(
     taskId INT,
     userId INT,
     dateTime DATETIME,
-    CONSTRAINT PK_TasksAssignations PRIMARY KEY (taskId, userId),
+    CONSTRAINT PK_TasksAssignations PRIMARY KEY (taskId, userId, dateTime),
+    CONSTRAINT FK_TasksAssignations_Task FOREIGN KEY (taskId) REFERENCES Tasks (id),
     CONSTRAINT FK_TasksAssignations_User FOREIGN KEY (userId) REFERENCES Users (id)
 );
 
@@ -34,9 +31,9 @@ CREATE TABLE TasksAssignations(
 CREATE TABLE TasksHistory(
     taskId INT,
     userId INT,
-    dateTime DATETIME,
+    dateTime DATETIME DEFAULT GETDATE(),
     action CHAR(1) NOT NULL,
-    CONSTRAINT PK_TasksHistory PRIMARY KEY (taskId, userId),
+    CONSTRAINT PK_TasksHistory PRIMARY KEY (taskId, userId, dateTime),
     CONSTRAINT FK_TasksHistory_Users FOREIGN KEY (userId) REFERENCES Users (id),
     CONSTRAINT FK_TasksHistory_Tasks FOREIGN KEY (taskId) REFERENCES Tasks (id),
     CONSTRAINT CHK_TasksHistory_action CHECK (action IN ('C', 'D', 'U'))
