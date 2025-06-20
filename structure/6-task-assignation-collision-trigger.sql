fcc75bc494f67e6bc5b70b2d21c85b7953f091e5
desmoris <damianesmoris@gmail.com>
2025-06-20 00:14:08 -0300

    Corrección al trigger para verificar asignaciones solapadas

--- structure/6-task-assignation-collision-trigger.sql
+++ structure/6-task-assignation-collision-trigger.sql
@@ -8,19 +8,14 @@ ON TasksAssignations
 INSTEAD OF INSERT
 AS
 BEGIN
-    DECLARE @userId INT;
-    SELECT
-        @userId = userId
-    FROM inserted;
-
     IF EXISTS (
         SELECT 1 FROM inserted i
         INNER JOIN TasksAssignations a
-        ON i.taskId = a.taskId AND i.userId = a.userId AND i.dateTime = a.dateTime
-        WHERE i.userId <> @userId
+        ON i.taskId = a.taskId AND i.dateTime = a.dateTime
+        WHERE i.userId <> a.userId
     )
     BEGIN
-        RAISERROR('A task cannot be assigned to different users at the same date and time.', 16, 1);
+        RAISERROR('A task cannot be assigned to different users at the same date time.', 16, 1);
         RETURN;
     END
 
