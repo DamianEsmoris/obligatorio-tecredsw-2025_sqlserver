cdb11c5c034d5400e721b92a2c5e1e68e649806b
desmoris <damianesmoris@gmail.com>
2025-07-07 03:44:12 -0300

    Test ajustados para probar la tabla `Task`

--- tests/1-tasks.sql
+++ tests/1-tasks.sql
@@ -5,11 +5,18 @@ GO
 
 GetTask 21;
 GO
-UpdateTask 21, 'Hacer cosas', 'Muchas cosas', '2025-11-28 10:00:00', 'Done';
+
+UpdateTask 21, 'Hacer cosas', 'Muchas cosas', 'Done';
 GO
+
 GetTask 21;
 GO
+
 DeleteTask 21;
 GO
+
+GetTaskHistoryByTaskId 21;
+GO
+
 GetAllTasks;
 GO
